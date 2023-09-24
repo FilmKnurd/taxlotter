ea190cf8c0082a599aad773f2172d25b2c1e1640
FilmKnurd <andrew@andrewelster.com>
2023-09-24 06:59:23 -0400

    Adds process_lots reducer with handler for buy trades

--- lib/operators.ex
+++ lib/operators.ex
@@ -2,6 +2,7 @@ defmodule TaxLotter.Operators do
   @moduledoc """
   Stream operations to process tades into tax lots
   """
+  alias Decimal, as: D
   alias TaxLotter.InvalidTrade
   alias TaxLotter.Trade
 
@@ -19,5 +20,40 @@ defmodule TaxLotter.Operators do
       {:ok, trade} -> trade
       {:error, changeset} -> raise InvalidTrade, changeset
     end
+  end
+
+  def process_lots(%{type: :buy} = trade, lot_acc) do
+    lots =
+      case lot_exists?(lot_acc.lots, trade.date) do
+        true -> update_lot(trade, lot_acc)
+        false -> build_lot(trade, lot_acc)
+      end
+
+    %{lot_acc | id: lot_acc.id + 1, lots: lots}
+  end
+
+  def process_lots(%{type: :sell} = trade, lot_acc) do
+  end
+
+  defp lot_exists?(lots, date) do
+    case Enum.find(lots, fn lot ->
+           Date.compare(lot.date, date) == :eq
+         end) do
+      nil -> false
+      _ -> true
+    end
+  end
+
+  defp build_lot(trade, %{id: id, lots: lots}), do: [Map.merge(trade, %{id: id + 1}) | lots]
+
+  defp update_lot(trade, %{lots: lots}) do
+    update_in(
+      lots,
+      [Access.filter(&(Date.compare(&1.date, trade.date) == :eq))],
+      &Map.merge(&1, %{
+        qty: D.add(trade.qty, &1.qty),
+        price: D.div(D.add(trade.price, &1.price), D.add(trade.qty, &1.qty))
+      })
+    )
   end
 end
--- test/operators_test.exs
+++ test/operators_test.exs
@@ -3,6 +3,7 @@ defmodule TaxLotter.OperatorsTest do
 
   alias TaxLotter.InvalidTrade
   alias TaxLotter.Operators
+  alias TaxLotter.Trade
 
   describe "remove_blanks/1" do
     test "returns false if passed list with empty binary" do
@@ -41,6 +42,58 @@ defmodule TaxLotter.OperatorsTest do
       assert_raise InvalidTrade, fn ->
         Operators.validate_trade(given_trade)
       end
+    end
+  end
+
+  describe "process_lots/2" do
+    test "creates a new lot for a buy trade" do
+      {:ok, given_trade} =
+        Trade.create(%{
+          line: 1,
+          date: "2023-07-21",
+          type: "buy",
+          price: "100.00",
+          qty: "1.00000000"
+        })
+
+      given_acc = %{id: 0, algo: :fifo, lots: []}
+
+      %{lots: [lot]} = Operators.process_lots(given_trade, given_acc)
+
+      assert lot.id == 1
+      assert Decimal.compare(given_trade.price, lot.price) == :eq
+      assert Decimal.compare(given_trade.qty, lot.qty) == :eq
+    end
+
+    test "aggregates buy trades on the same lot by date" do
+      {:ok, given_trade1} =
+        Trade.create(%{
+          line: 1,
+          date: "2023-07-21",
+          type: "buy",
+          price: "100.00",
+          qty: "1.00000000"
+        })
+
+      {:ok, given_trade2} =
+        Trade.create(%{
+          line: 2,
+          date: "2023-07-21",
+          type: "buy",
+          price: "200.00",
+          qty: "1.00000000"
+        })
+
+      given_acc = %{id: 0, algo: :fifo, lots: []}
+      expected_price = Decimal.new("150.00")
+      expected_qty = Decimal.new("2.00000000")
+
+      %{lots: [lot]} =
+        Enum.reduce([given_trade1, given_trade2], given_acc, &Operators.process_lots/2)
+
+      assert lot.id == 1
+      assert Decimal.compare(expected_price, lot.price) == :eq
+      assert Decimal.compare(expected_qty, lot.qty) == :eq
     end
   end
 end
